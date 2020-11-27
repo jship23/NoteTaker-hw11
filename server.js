1b0bc6ecc1999a0e5698c9495f6dba3071ee1537
Jordan Shipley <jordan.j.shipley@gmail.com>
2020-11-27 15:07:01 -0700

    starter code

---
+++ server.js
@@ -0,0 +1,10 @@
+//dependencies
+
+const fs = require("fs");
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
