3af9e127e36303b66a2cf56c1615fe6e9854d27f
Jordan Shipley <jordan.j.shipley@gmail.com>
2020-11-29 17:04:10 -0700

    get html routes

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 //dependencies
 
 const fs = require("fs");
+const path = require("path");
 const express = require("express");
 
 const app = express();
@@ -8,3 +9,17 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+
+//GET
+app.get("/notes",  (req, res) => {
+    res.sendFile(path.join(__dirname, "notes.html"));
+});
+
+app.get("*", (req, res) =>{
+    res.sendFile(path.join(__dirname, "index.html"))
+});
+
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+  });
