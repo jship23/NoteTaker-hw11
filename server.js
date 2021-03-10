9c9f0928b594600ba79d12c7949d886f5686f439
Jordan Shipley <jordan.j.shipley@gmail.com>
2021-03-09 18:00:36 -0700

    created store.js

--- server.js
+++ server.js
@@ -1,22 +1,49 @@
 //dependencies
-
 const fs = require("fs");
 const path = require("path");
 const express = require("express");
+const store = require("./db/store");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+app.use(express.static("public"));
 
-//GET
+//GET HTML ROUTES
 app.get("/notes",  (req, res) => {
-    res.sendFile(path.join(__dirname, "notes.html"));
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
 app.get("*", (req, res) =>{
-    res.sendFile(path.join(__dirname, "index.html"))
+    res.sendFile(path.join(__dirname, "/public/index.html"))
+});
+
+//GET API ROUTES
+app.get("/api/notes", (req, res) =>{
+    store
+    .getNotes()
+    .then((notes) => res.json(notes))
+    .catch((err) => res.status(500).json(err));
+});
+
+//POST
+app.post("/api/notes", (req, res) =>{
+    store
+    .addNote(req.body)
+    .then((note) => res.json(note))
+    .catch((err) => res.status(500).json(err));
+});
+
+//DELETE
+app.delete("/api/notes/:id", function(req, res) {
+    store
+    .removeNote(req.params.id)
+    .then(() => res.json({ ok: true }))
+    .catch((err) => res.status(500).json(err));
+
 });
 
 
